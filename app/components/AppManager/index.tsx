130ad450dbf111c3e571cf4e6915f8377d5ded1b
Tejas Gajjar <gajjartejas26@gmail.com>
2025-03-30 21:55:47 +0530

    fix: in-app purchase

--- app/components/AppManager/index.tsx
+++ app/components/AppManager/index.tsx
@@ -1,5 +1,5 @@
 import React, { memo, ReactElement, useEffect } from 'react';
-import { Appearance, View } from 'react-native';
+import { Alert, Appearance, EmitterSubscription, View } from 'react-native';
 
 //App Modules
 import styles from './styles';
@@ -7,8 +7,19 @@ import AppearancePreferences = Appearance.AppearancePreferences;
 import useThemeConfigStore, { IAppearanceType } from 'app/store/themeConfig';
 import i18n from 'app/locales';
 import useAppLangConfigStore from 'app/store/appLangConfig';
-import { getAvailablePurchases, initConnection } from 'react-native-iap';
+import {
+  finishTransaction,
+  flushFailedPurchasesCachedAsPendingAndroid,
+  getAvailablePurchases,
+  initConnection,
+  ProductPurchase,
+  purchaseErrorListener,
+  purchaseUpdatedListener,
+} from 'react-native-iap';
 import useAppConfigStore from 'app/store/appConfig';
+import { navigationRef } from 'app/navigation/NavigationService';
+import { useTranslation } from 'react-i18next';
+import crashlytics from '@react-native-firebase/crashlytics';
 
 //Interface
 export type Props = {
@@ -20,17 +31,59 @@ const AppManager = ({ children }: Props) => {
   const appearance = useThemeConfigStore(store => store.appearance);
   const setPurchased = useAppConfigStore(state => state.setPurchased);
   const selectedLanguageCode = useAppLangConfigStore(store => store.selectedLanguageCode);
+  const { t } = useTranslation();
 
   //States
+
   useEffect(() => {
+    let purchaseUpdateSubscription: EmitterSubscription;
+    let purchaseErrorSubscription: EmitterSubscription;
+
     (async () => {
       await initConnection();
+      await flushFailedPurchasesCachedAsPendingAndroid();
+
       const purchases = await getAvailablePurchases();
       if (purchases && purchases.length > 0) {
-        setPurchased(true);
+        setPurchased(__DEV__ ? false : true);
       }
+
+      purchaseUpdateSubscription = purchaseUpdatedListener(async (purchase: ProductPurchase) => {
+        const receipt = purchase.transactionReceipt;
+        console.log('AppManager->useEffect->purchaseUpdatedListener->receipt', receipt);
+        if (receipt) {
+          try {
+            const ackResult = await finishTransaction({ purchase, isConsumable: false });
+            console.log('AppManager->useEffect->purchaseUpdatedListener->ackResult', ackResult);
+            setPurchased(true);
+            Alert.alert(t('iap_purchased_success'));
+            navigationRef.current?.goBack();
+          } catch (e: any) {
+            console.error('AppManager->useEffect->purchaseUpdatedListener->error:', e);
+            Alert.alert(e.message);
+            crashlytics().recordError(e, 'AppManager->useEffect->purchaseUpdatedListener->error');
+          }
+        } else {
+          console.warn('AppManager->useEffect->purchaseUpdatedListener->receipt->not found');
+        }
+      });
+
+      purchaseErrorSubscription = purchaseErrorListener(e => {
+        console.log('purchaseErrorListener', e);
+        Alert.alert(e.message);
+        crashlytics().recordError(e, 'AppManager->useEffect->purchaseErrorListener');
+      });
     })();
-  }, [setPurchased]);
+
+    return () => {
+      if (purchaseUpdateSubscription) {
+        purchaseUpdateSubscription.remove();
+      }
+      if (purchaseErrorSubscription) {
+        purchaseErrorSubscription.remove();
+      }
+    };
+  }, [setPurchased, t]);
 
   useEffect(() => {
     i18n.changeLanguage(selectedLanguageCode).then(() => {});
